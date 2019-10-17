b6c54d691aaf31f84feef59309a00d928d889c9c
vangasse <vangasse@ic.ufal.br>
2019-10-17 13:47:34 -0300

    Computer Assignment 1 - Correction

--- Computer_Assignment_C2_1.m
+++ Computer_Assignment_C2_1.m
@@ -6,9 +6,9 @@ disp(['(a) k=3: roots = [', num2str(r.'),']']);
 k = 4;
 r = roots([1 4 k]);
 
-disp(['(a) k=4: roots = [', num2str(r.'),']']);
+disp(['(b) k=4: roots = [', num2str(r.'),']']);
 
 k = 40;
 r = roots([1 4 k]);
 
-disp(['(a) k=40: roots = [', num2str(r.'),']']);
+disp(['(c) k=40: roots = [', num2str(r.'),']']);
