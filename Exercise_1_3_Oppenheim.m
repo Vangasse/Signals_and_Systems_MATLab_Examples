6371cc55d27b6cadb54f8af76483c83513226c1e
vangasse <vangasse@ic.ufal.br>
2019-10-17 08:42:09 -0300

    Ex3 Oppenheim OK

--- Exercise_1_3_Oppenheim.m
+++ Exercise_1_3_Oppenheim.m
@@ -1,2 +1,13 @@
-n = 1:100;
-x2_n = exp(i.*(pi./(2*n) + pi./8));
+eval clear;
+x2_n = [];
+E = [];
+for n = -100:100
+    if n ~= 0
+        x_curr = exp(i.*(pi/(2*n) + pi/8));
+        x2_n = [x2_n x_curr];
+        E = [E abs(x_curr)^2];
+        E = sum(E);
+        P = E/(2*n + 1);
+    end
+end
+
